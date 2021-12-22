9b8867f88c51789580e17fa27bf9e94088199325
muhammed.s.elsayed <muhammed.s.elsayed@gmail.com>
2021-12-22 15:48:06 -0500

    Got left pane html for content up with text

--- src/components/HtmlBS/BoxContent.js
+++ src/components/HtmlBS/BoxContent.js
@@ -5,29 +5,23 @@ export default function BoxContent(props) {
   let text;
   useEffect(() => {
     console.log(context)
-    
+
   }, [context])
 
 
 
   return (
     <>
- 
+
       <div className="pointer-events-none">
-
-  
-        <div className=" flex relative h-screen items-center justify-center">
-          <div className={`bg-auto bg-green-700 bg-content h-screen  ${context.text.enable ? "animate-fade-in" : "animate-fade-out"}`}>
-
-            <span className={`m-auto  ${context.text.enable ? "animate-fade-in" : "animate-fade-out"}`}>sadasd</span>
-    
-            
-
+        <div className={`h-screen bg-gray-800 bg-opacity-50 w-1/3 right-0 fixed z-50 ${context.text.enable ? "animate-fade-in" : "animate-fade-out"}`}>
+          <div className=" flex relative items-center z-10 justify-center h-screen">
+              
+              <span className={`m-auto  ${context.text.enable ? "animate-fade-in" : "animate-fade-out"}`}>sadasd</span>
+            </div>
           </div>
         </div>
-        <div className="flex absolute h-screen bg-cover bg-center z-10 bg-indigo-800 w-1/3 right-0"><span>asdas</span></div>
-        </div>
-   
+
     </>
   );
 
